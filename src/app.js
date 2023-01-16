81af40b5790450d3d39bba0b9b384d11ccfcd1cd
Dev-organico <joao.zolet@gmail.com>
2023-01-15 21:15:03 -0300

    hub test

--- src/app.js
+++ src/app.js
@@ -85,6 +85,7 @@ app.get('/participants', async (req, res) => {
         const participantsList = await db.collection("participants").find().toArray()
 
         res.send(participantsList)
+
     } catch (err) {
         return res.status(500).send(err.message)
     }
@@ -173,6 +174,25 @@ app.get('/messages', async (req, res) => {
 })
 
 app.post('/status', async (req, res) => {
+
+    const user = req.headers.user
+
+    try {
+        const alreadyExist = await db.collection("participants").findOne({ name:user })
+
+        const id = alreadyExist._id
+
+        if (!alreadyExist) return res.sendStatus(404)
+
+        await db.collection("participants").updateOne({ name:user },{$set:{lastStatus:Date.now()}})
+
+        res.sendStatus(200)
+
+    } catch (err) {
+
+        return res.status(500).send(err.message)
+
+    }
 
 
 })
